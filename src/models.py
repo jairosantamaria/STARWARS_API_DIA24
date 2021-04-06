2966ae5d6e0f4f96224e90b053687b67fa3d59eb
jairosantamaria <jairosantamaria@amelearning.net>
2021-04-06 03:04:59 +0000

    rev03

--- src/models.py
+++ src/models.py
@@ -1,9 +1,58 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    nickname = db.Column(String(250), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    is_active = db.Column(db.Boolean(), nullable=False)
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    diameter = db.Column(db.Integer, nullable=False)
+    rotation_period = db.Column(db.Integer, nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=False)
+    gravity = db.Column(db.String(250), nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+    terrain = db.Column(db.String(250), nullable=False)
+    surface_water = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+
+def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+       }
+#nueva clase
+class People(Base):
+    __tablename__ = 'people'
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String(250), nullable=False)
+    height = db.Column(Integer, nullable=False)
+    mass = db.Column(Integer, nullable=False)
+    hair_color = db.Column(String(250), nullable=False)
+    skin_color = db.Column(String(250), nullable=False)
+    birth_year = db.Column(Integer, nullable=False)
+    gender = db.Column(String(250), nullable=False)
+    home_world = db.Column(String(250), nullable=False)
+    planet_id = db.Column(Integer, nullable=False)
